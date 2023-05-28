0e2acb38afc74d6d7b90bba46fb9421d0c2202f4
kau14376 <kau14376@sheridancollege.ca>
2023-05-28 17:07:45 -0400

    ICE 1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -4,11 +4,13 @@
  */
 package card;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+ * @author Taranjot kaur
  */
 public class CardTrick {
     
@@ -19,12 +21,26 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random()*13+1));
+            c.setSuit(Card.SUITS[(int)(Math.random()*3+0)]);
+            magicHand[i]=c;
+            System.out.println(magicHand[i].getSuit()+" "+magicHand[i].getValue());
         }
         
         //insert code to ask the user for Card value and suit, create their card
+         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the card value (2-10, J, Q, K, A): ");
+        String value = scanner.nextLine();
+
+        System.out.print("Enter the card suit (hearts, diamonds, clubs, spades): ");
+        String suit = scanner.nextLine();
+
+        String card = value + " of " + suit;
+
+        System.out.println("Your card is: " + card);
         // and search magicHand here
+        
         //Then report the result here
     }
     
